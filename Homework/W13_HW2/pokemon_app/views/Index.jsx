fcf387cf0792837df99c3a8f4596e63eea26cfd0
geracia2 <geracia2@gmail.com>
2024-01-15 14:05:02 -0500

    dynamically replace name case

--- Homework/W13_HW2/pokemon_app/views/Index.jsx
+++ Homework/W13_HW2/pokemon_app/views/Index.jsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const inLineStyle = {
   color: "#ffffff",
   backgroundColor: "#000000",
 };
+// const pokemon = null
 
-export default function Index() {
+// RegEx
+// look at all lowercase letters,
+// look at all letters after ?! and space,
+// look globally
+const regEx = /(\b[a-z](?!\s))/g;
+// how to use it
+// __.replace(regEx, function(x){return x.toUpperCase();});
+
+export default function Index({ pokemon }) {
   return (
     <>
+      {console.log(Array.from({ length: 1 }))}
       <h1 style={inLineStyle}>See all the Pokemon!</h1>
+      {pokemon && (
+        <ul>
+          {pokemon.map((mon, i) => (
+            <Fragment key={mon.name}>
+              <li>
+                {mon.name.replace(regEx, function (x) {
+                  return x.toUpperCase();
+                })}
+                <ul>
+                  <li>
+                    {/* <img src={`${mon.img}.jpg`} alt="" /> */}
+                    {mon.name}
+                  </li>
+                </ul>
+              </li>
+            </Fragment>
+          ))}
+        </ul>
+      )}
+      {!pokemon && (
+        <>
+          <h1>Oh no, where are the pokemon?</h1>
+        </>
+      )}
     </>
   );
 }
